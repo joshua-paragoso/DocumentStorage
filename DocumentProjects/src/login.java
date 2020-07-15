79922fb4c2e7c166c456a5d5dadf54393226d357
joshypuu <jparagos@asu.edu>
2020-07-15 11:38:08 -1000

    Added a login page

--- DocumentProjects/src/login.java
+++ DocumentProjects/src/login.java
@@ -1,11 +1,19 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class login {
 
 	private JFrame frame;
-//Test
+	private JTextField textField;
+	private JTextField textField_1;
+
 	/**
 	 * Launch the application.
 	 */
@@ -36,6 +44,32 @@ public class login {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Login");
+		lblNewLabel.setBounds(198, 24, 61, 16);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setBounds(89, 116, 73, 16);
+		frame.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(89, 167, 73, 21);
+		frame.getContentPane().add(lblPassword);
+		
+		textField = new JTextField();
+		textField.setBounds(161, 111, 130, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(161, 164, 130, 26);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(161, 221, 117, 29);
+		frame.getContentPane().add(btnLogin);
 	}
-
 }
