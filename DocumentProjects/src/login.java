bc18b105d9ec6c8534dd60f677e5e222503829df
joshypuu <jparagos@asu.edu>
2020-07-15 15:58:18 -1000

    Got the login screen to take in user info and able to hide password

--- DocumentProjects/src/login.java
+++ DocumentProjects/src/login.java
@@ -1,9 +1,15 @@
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -11,8 +17,8 @@ import javax.swing.JButton;
 public class login {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField username;
+	private JPasswordField password;
 
 	/**
 	 * Launch the application.
@@ -54,21 +60,36 @@ public class login {
 		lblUsername.setBounds(89, 116, 73, 16);
 		frame.getContentPane().add(lblUsername);
 		
+		username = new JTextField();
+		username.setBounds(161, 111, 130, 26);
+		frame.getContentPane().add(username);
+		username.setColumns(10);
+		
 		JLabel lblPassword = new JLabel("Password:");
 		lblPassword.setBounds(89, 167, 73, 21);
 		frame.getContentPane().add(lblPassword);
-		
-		textField = new JTextField();
-		textField.setBounds(161, 111, 130, 26);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(161, 164, 130, 26);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+	    
+		password = new JPasswordField();
+		password.setBounds(161, 164, 130, 26);
+		frame.getContentPane().add(password);
+		password.setColumns(10);
 		
 		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if( username.getText().isEmpty() || password.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Please enter username and password");
+				}else if(username.getText().equals("admin") && password.getText().contentEquals("12345")){
+					JOptionPane.showMessageDialog(null, "login success");
+				}else {
+					JOptionPane.showMessageDialog(null, "login failed. Retry");
+				}
+			} 
+		
+		});
 		btnLogin.setBounds(161, 221, 117, 29);
 		frame.getContentPane().add(btnLogin);
 	}
