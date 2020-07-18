d7bf8c28c59418054556c460615afa7015fb4810
joshypuu <jparagos@asu.edu>
2020-07-17 17:03:02 -1000

    Edited the home.java to load files

--- DocumentProjects/src/home/Home.java
+++ DocumentProjects/src/home/Home.java
@@ -6,12 +6,37 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JTextField;
+import java.awt.Scrollbar;
+import javax.swing.JScrollPane;
+import javax.swing.JInternalFrame;
+import javax.swing.JDesktopPane;
+import javax.swing.JFileChooser;
+import javax.swing.JScrollBar;
+import javax.swing.JTable;
+import java.awt.List;
+import java.awt.Component;
+
+import javax.imageio.ImageIO;
+import javax.swing.Box;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
 
 public class Home extends JFrame {
-
-	private JPanel contentPane;
-	private JTextField txtHomePage;
+	
+	//JFile Choosers
+	private final JFileChooser openFileChooser; 
+	private BufferedImage originalBI;
 
 	/**
 	 * Launch the application.
@@ -33,20 +58,39 @@ public class Home extends JFrame {
 	 * Create the frame.
 	 */
 	public Home() {
+		
+		openFileChooser = new JFileChooser();
+		openFileChooser.setCurrentDirectory(new File("/Users/joshypuu"));
+//		openFileChooser.setFileFilter(new FileNameExtensionFilter("PNG images, png"));
+//		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setBounds(100, 100, 450, 400);
+		getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
+		JLabel messageLabel = new JLabel("New label");
+		messageLabel.setBounds(135, 23, 285, 24);
+		getContentPane().add(messageLabel);
 		
-		txtHomePage = new JTextField();
-		txtHomePage.setText("Home page");
-		panel.add(txtHomePage);
-		txtHomePage.setColumns(10);
+		JButton btnOpenFile = new JButton("Open file");
+		btnOpenFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int returnValue = openFileChooser.showOpenDialog(null);
+				
+				if(returnValue == JFileChooser.APPROVE_OPTION) { 
+					try {
+						originalBI = ImageIO.read(openFileChooser.getSelectedFile());
+						messageLabel.setText("Image file successfully loaded");
+					}catch (IOException ioe) {
+						messageLabel.setText("File find fail");
+					}
+				}else {
+					messageLabel.setText("No file chosen!");
+				}
+			}
+		});
+		btnOpenFile.setBounds(6, 22, 117, 29);
+		getContentPane().add(btnOpenFile);
+		
+		
 	}
-
 }
